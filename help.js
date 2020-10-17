761416a945d056431834b0c9cfe97b835240ef44
Mathias Buus <mathiasbuus@gmail.com>
2020-10-18 01:28:36 +0200

    Create help.js

---
+++ help.js
@@ -0,0 +1,89 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const { spawn } = require('child_process')
+const cors = require('cors')
+
+if (!process.argv[2]) createServer('out')
+else createHelper(process.argv[2], Number(process.argv[3]))
+
+function createServer (dir) {
+  const chunks = fs.readdirSync(dir).filter(name => name.endsWith('.seg'))
+  const all = cors()
+  const server = http.createServer(function (req, res) {
+    all(req, res, function () {
+      if (req.method === 'GET') {
+        const chunk = chunks.shift()
+        if (!chunk) {
+          res.statusCode = 404
+          res.end()
+          return
+        }
+        const filename = path.join(dir, chunk)
+
+        fs.readFile(filename, function (err, buf) {
+          res.setHeader('X-Chunk', chunk)
+          res.end(buf)
+        })
+        return
+      }
+      if (req.method === 'PUT') {
+        const buf = []
+        req.on('data', function (data) {
+          buf.push(data)
+        })
+        req.on('end', function () {
+          const chunk = req.headers['x-chunk']
+          if (!chunk) return res.end()
+          console.log('Writing chunk', chunk)
+          fs.writeFile(path.join(dir, chunk + '.ts'), Buffer.concat(buf), function () {
+            res.end()
+          })
+        })
+      }
+    })
+  })
+
+  server.listen(8080)
+}
+
+function createHelper (host, port) {
+  console.log({ host, port })
+  work()
+
+  function work () {
+    const req = http.request({
+      method: 'GET',
+      port,
+      host
+    })
+    req.end()
+    req.on('response', function (res) {
+      if (res.statusCode === 404) return console.log('Done...')
+      console.log('Got chunk', res.headers['x-chunk'])
+
+      const req = http.request({
+        method: 'PUT',
+        headers: {
+          'x-chunk': res.headers['x-chunk']
+        },
+        port,
+        host
+      })
+
+      req.on('response', function (res) {
+        res.resume()
+        work()
+      })
+
+      var proc = spawn('ffmpeg', ['-i', '-', '-f', 'mpegts', '-y', '-vcodec', 'h264', '-acodec', 'aac', '-'], { stdio: ['pipe', 'pipe', 'inherit'] })
+
+      proc.on('exit', function (code) {
+        if (code) process.exit(code)
+      })
+
+      res.pipe(proc.stdin)
+      proc.stdout.pipe(req)
+    })
+  }
+}
